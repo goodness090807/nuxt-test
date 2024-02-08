3c7af4e0f434f881180d8c8b3c44f6f4c91c1dff
Terry <b0972834939@gmail.com>
2024-02-08 23:51:46 +0800

    feat: init project

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <button @click="navigate">page1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        navigate() {},
+    },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button @click="navigate">page2</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        navigate() {
+            navigateTo("/about");
+        },
+    },
+    mounted() {
+        const originalPushState = history.pushState;
+        const originalReplaceState = history.replaceState;
+
+        history.pushState = function () {
+            console.log("history.pushState triggered", arguments);
+            originalPushState.apply(this, arguments);
+        };
+
+        history.replaceState = function () {
+            console.log("history.replaceState triggered", arguments);
+            originalReplaceState.apply(this, arguments);
+        };
+    },
+};
+</script>

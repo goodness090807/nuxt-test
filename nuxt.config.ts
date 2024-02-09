6aa145ea445e106c11d3540f78a33e0bbf936513
Terry <b0972834939@gmail.com>
2024-02-09 11:02:27 +0800

    feat: 增加GTM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    modules: ["nuxt-gtag", "@zadigetvoltaire/nuxt-gtm"],
+    gtag: {
+        id: "G-17NB8M1C1V",
+    },
+    gtm: {
+        id: "GTM-56G726ML",
+    },
+});
